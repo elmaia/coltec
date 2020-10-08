8cb22ed833f779839b87f014b85adea93246d829
Leandro <elmaia@coltec.ufmg.br>
2020-10-08 11:39:18 -0300

    Arquivo com as definições de erros.

---
+++ IP/Solucao Exercicios/erros.h
@@ -0,0 +1,16 @@
+/**
+* @file   erros.h
+* @brief  Arquivo com a definição dos possíveis erros previstos no programa.
+* @author Leandro Maia Silva
+* @date   2019-04-13
+*/
+#pragma once
+
+#ifndef __ERROS_h__
+#define __ERROS_h__
+
+#define SUCESSO 0
+
+
+#endif /* __ERROS_h__ */
+
