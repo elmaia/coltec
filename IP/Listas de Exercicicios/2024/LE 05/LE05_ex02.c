cca8025f925e3fdd4f99e924a303a64694f19e11
Leandro <elmaia@coltec.ufmg.br>
2024-05-26 15:18:17 -0300

    Lista de Exercícios 2024 - 05
    - Implementação do Exercício 02
    - Disponibilização do arquivo de testes do exercício 02 para o testador.py

--- IP/Listas de Exercicicios/2024/LE 05/LE05_ex02.c
+++ IP/Listas de Exercicicios/2024/LE 05/LE05_ex02.c
@@ -28,13 +28,40 @@
 
 // ----------------------------------------------------------------------
 
+#define DIAS_ANO  365
+#define DIAS_MES   30
+
+int idadeEmDias(int anos, int meses, int dias) {
+  return anos * DIAS_ANO + meses * DIAS_MES + dias;
+}
+
 /**
  *  Faça uma função que receba a idade de uma pessoa em
  *  anos, meses e dias e retorna essa idade expressa em dias.
  *  Considere cada ano como 365 dias e cada mês como 30 dias.
  */
 int main(int argc, char ** argv) {
+    
+  int anos, meses, dias;
+  int idadeDias;
   
+  // Obtem os dados do usuário
+  printf("Calculadora de dias de vida.\n");
+  printf("Digite a quantidade de anos, meses e dias desde o seu nascimento.\n");
+  printf("Anos: ");
+  scanf("%d", &anos);
+  printf("Meses: ");
+  scanf("%d", &meses);
+  printf("Dias: ");
+  scanf("%d", &dias);
+  
+  // Calcula o número de dias
+  idadeDias = idadeEmDias(anos, meses, dias);
+  
+  // Mostra o resultado para o usuário
+  printf("Voce ja viveu ");
+  PRINTF("%d", idadeDias);
+  printf(" dias desde o seu nascimento.\n");
   // Se chegou até aqui é porque correu tudo bem
   return SUCESSO;
 }
