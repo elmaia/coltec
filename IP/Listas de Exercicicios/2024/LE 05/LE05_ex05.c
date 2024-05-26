eff7da19231c14a7036dff36aa714b21863edcba
Leandro <elmaia@coltec.ufmg.br>
2024-05-26 15:48:08 -0300

    Lista de Exercícios 2024 - 05
    - Implementação do Exercício 05
    - Disponibilização do arquivo de testes do exercício 05 para o testador.py

--- IP/Listas de Exercicicios/2024/LE 05/LE05_ex05.c
+++ IP/Listas de Exercicicios/2024/LE 05/LE05_ex05.c
@@ -8,6 +8,7 @@
 
 /* Inclusões */
 #include <stdio.h>   // printf e scanf
+#include <stdint.h>  // Para usar uint64_t ao invés de unsigned long long int
 
 /* Constantes */
 
@@ -28,11 +29,40 @@
 
 // ----------------------------------------------------------------------
 
+uint64_t fatorial(int n) {
+  
+  int i;
+  uint64_t fat = 1;
+  
+  for(i = 1; i <= n; i++) {
+    fat*=i;
+  }
+  
+  return fat;
+}
+
 /**
  *  Faça uma função que receba um valor N inteiro e positivo
  *  e que calcula o fatorial deste valor.
  */
 int main(int argc, char ** argv) {
+    
+  uint64_t fat;
+  int numero;
+  
+  // Obtem o número para calcular o fatorial
+  printf("Calculadora de Fatorial.\n");
+  printf("Digite o numero para calcular o fatorial.\n");
+  printf(":> ");
+  scanf("%d", &numero);
+  
+  // Calcula numero!
+  fat = fatorial(numero);
+  
+  // Mostra o resultado
+  printf("%d! eh ", numero);
+  PRINTF("%llu", fat);
+  printf("\n");
   
   // Se chegou até aqui é porque correu tudo bem
   return SUCESSO;
