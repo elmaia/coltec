811ec2d7b2cedc0be000f6d688062e471dae1293
Leandro <elmaia@coltec.ufmg.br>
2024-05-26 16:53:06 -0300

    Lista de Exercícios 2024 - 05
    - Implementação do Exercício 07
    - Disponibilização do arquivo de testes do exercício 07 para o testador.py

--- IP/Listas de Exercicicios/2024/LE 05/LE05_ex07.c
+++ IP/Listas de Exercicicios/2024/LE 05/LE05_ex07.c
@@ -8,6 +8,7 @@
 
 /* Inclusões */
 #include <stdio.h>   // printf e scanf
+#include <stdbool.h> // bool, true e false
 
 /* Constantes */
 
@@ -28,6 +29,22 @@
 
 // ----------------------------------------------------------------------
 
+/* Constantes */
+
+// Número de notas dos alunos
+#define NUMERO_NOTAS 3
+
+// Nota mínima para aprovação
+#define APROVACAO_NOTA_MINIMA  60.0f
+
+float calculaMedia(float nota1, float nota2, float nota3) {
+  return (nota1 + nota2 + nota3) / NUMERO_NOTAS;
+}
+
+bool estaAprovado(float nota) {
+  return (nota >= APROVACAO_NOTA_MINIMA);
+}
+
 /**
  *  Faça uma função que recebe três notas de um aluno e
  *  calcula sua média. Também faça uma função que verifica se um aluno foi
@@ -38,6 +55,55 @@
  *  Para contar como aprovado, a nota deve ser igual ou superior à 60 pontos.
  */
 int main(int argc, char ** argv) {
+    
+  int i;
+  int nAlunos;
+  int nAlunosAprovados = 0;
+  float nota1, nota2, nota3;
+  float media;
+  float percAprovacao;
+  
+  printf("Notas dos Alunos.\n");
+  printf("Digite o numero de alunos: ");
+  scanf("%d%*c", &nAlunos);
+  
+  // Obtem as notas dos alunos
+  for(i = 0; i < nAlunos; i++) {
+    printf("Notas do aluno %d.\n", i + 1);
+    printf("Nota 01: ");
+    scanf("%f%*c", &nota1);
+    printf("Nota 02: ");
+    scanf("%f%*c", &nota2);
+    printf("Nota 03: ");
+    scanf("%f%*c", &nota3);
+    
+    // Obtem a média
+    media = calculaMedia(nota1, nota2, nota3);
+    
+    // Verifica se foi aprovado
+    if (estaAprovado(media)) {
+      nAlunosAprovados++;
+    }
+  }
+  
+  // Calcula o percentual de aprovação
+  percAprovacao = ((float)nAlunosAprovados / nAlunos) * 100;
+  
+  // Mostra as estatisticas
+  printf("Estatisticas.\n");
+  printf("N alunos....: ");
+  PRINTF("%4d", nAlunos);
+  printf("\n");
+  printf("N aprovados.:");
+  PRINTF(" %4d", nAlunosAprovados);
+  printf(" | Perc:");
+  PRINTF(" %6.2f", percAprovacao);
+  printf("\n");
+  printf("N reprovados:");
+  PRINTF(" %4d", nAlunos - nAlunosAprovados);
+  printf(" | Perc:");
+  PRINTF(" %6.2f", 100.0f - percAprovacao);
+  printf("\n");
   
   // Se chegou até aqui é porque correu tudo bem
   return SUCESSO;
